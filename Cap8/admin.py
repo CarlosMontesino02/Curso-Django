4f0897bf321833140de96a77769212fe6133d251
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2021-05-11 11:15:08 +0200

    Update admin.py

--- Cap8/admin.py
+++ Cap8/admin.py
@@ -5,7 +5,7 @@ from .models import Registrado
 class AdminRegistrado(admin.ModelAdmin):
     list_display = ["email","nombre", "timestamp"]
     list_filter = ["timestamp"]
-    list_display_links=["nombre"]
+    list_display_links=["email"]
     list_editable = ["nombre"]
     search_fields = ["email", "nombre"]
     class Meta:
