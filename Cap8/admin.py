29303b805b028c2a752599e761dcd1e1af9575da
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2021-05-11 11:03:45 +0200

    Add files via upload

---
+++ Cap8/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Registrado
+# Register your models here.
+class AdminRegistrado(admin.ModelAdmin):
+    list_display = ["email","nombre", "timestamp"]
+    list_filter = ["timestamp"]
+    list_display_links=["nombre"]
+    list_editable = ["nombre"]
+    search_fields = ["email", "nombre"]
+    class Meta:
+        model = Registrado
+
+
+
+admin.site.register(Registrado, AdminRegistrado)
